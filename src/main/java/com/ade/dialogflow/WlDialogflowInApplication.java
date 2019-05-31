111305429ff294c1aee1c8047f5064fbed7b8a28
Ade Priyanto <ade.priyanto06@gmail.com>
2019-05-31 10:13:51 +0700

    push to github

---
+++ src/main/java/com/ade/dialogflow/WlDialogflowInApplication.java
@@ -0,0 +1,13 @@
+package com.ade.dialogflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WlDialogflowInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WlDialogflowInApplication.class, args);
+	}
+
+}
